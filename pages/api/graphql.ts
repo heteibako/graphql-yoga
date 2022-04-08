3f600c6fe42ba3077565b82ddc6d3453daa9b38d
Szabolcs Hetei-Bako <szabi.hetei-bako@bolindalabs.com>
2022-04-08 16:36:30 +0200

    test

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { createServer } from "@graphql-yoga/node";
-import { schema } from "@schemas/main";
 import { NextApiRequest, NextApiResponse } from "next";
+import { schema } from "@schemas/all";
 
 const server = createServer<{
   req: NextApiRequest;
